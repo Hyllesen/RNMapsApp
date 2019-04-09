6db80a07b954d1b011e84e7b938420c43da2bf95
Stefan Hyltoft <stefan@Stefans-iMac.local>
2019-04-09 18:57:47 +0200

    Now following user location in Android

--- MapScreen.js
+++ MapScreen.js
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps"; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView from "react-native-maps";
+
+export default class MapScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userLatitude: 0,
+      userLongitude: 0
+    };
+  }
+
+  componentDidMount() {
+    this.locationWatchId = navigator.geolocation.watchPosition(
+      pos => {
+        this.setState({
+          userLatitude: pos.coords.latitude,
+          userLongitude: pos.coords.longitude
+        });
+      },
+      err => console.warn(err),
+      {
+        enableHighAccuracy: true
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.locationWatchId);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          showsUserLocation
+          followsUserLocation
+          style={styles.map}
+          region={{
+            latitude: this.state.userLatitude,
+            longitude: this.state.userLongitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -13,20 +60,3 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject
   }
 });
-
-export default () => (
-  <View style={styles.container}>
-    <MapView
-      showsUserLocation
-      followsUserLocation
-      //provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121
-      }}
-    />
-  </View>
-);
