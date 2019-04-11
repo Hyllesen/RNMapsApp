be068f33850dbc004337034a9e4e8c37c0b66b7f
Stefan Hyltoft <stefan@Stefans-iMac.local>
2019-04-11 22:54:31 +0200

    Using debounce to limit api calls.

--- components/PlaceInput.js
+++ components/PlaceInput.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, TextInput } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Keyboard
+} from "react-native";
 import axios from "axios";
+import _ from "lodash";
 
 export default class PlaceInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      predictions: []
+      predictions: [],
+      destinationInput: ""
     };
     this.getPlaces = this.getPlaces.bind(this);
+    this.getPlacesDebounced = _.debounce(this.getPlaces, 1000);
+    this.setDestination = this.setDestination.bind(this);
   }
 
   async getPlaces(input) {
@@ -16,6 +27,11 @@ export default class PlaceInput extends Component {
       `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyBx4jdXqTV6sX6IiFx1lC50l_0b_32Neys&input=${input}&location=37.422356, -122.084068&radius=2000`
     );
     this.setState({ predictions: result.data.predictions });
+  }
+
+  setDestination(main_text) {
+    Keyboard.dismiss();
+    this.setState({ destinationInput: main_text, predictions: [] });
   }
 
   render() {
@@ -28,20 +44,31 @@ export default class PlaceInput extends Component {
     const predictions = this.state.predictions.map(prediction => {
       const { id, structured_formatting } = prediction;
       return (
-        <View key={id} style={suggestionStyle}>
-          <Text style={main_textStyle}>{structured_formatting.main_text}</Text>
-          <Text style={secondary_textStyle}>
-            {structured_formatting.secondary_text}
-          </Text>
-        </View>
+        <TouchableOpacity
+          key={id}
+          onPress={() => this.setDestination(structured_formatting.main_text)}
+        >
+          <View style={suggestionStyle}>
+            <Text style={main_textStyle}>
+              {structured_formatting.main_text}
+            </Text>
+            <Text style={secondary_textStyle}>
+              {structured_formatting.secondary_text}
+            </Text>
+          </View>
+        </TouchableOpacity>
       );
     });
     return (
       <View>
         <TextInput
+          value={this.state.destinationInput}
           autoCapitalize="none"
           autoCorrect={false}
-          onChangeText={input => this.getPlaces(input)}
+          onChangeText={input => {
+            this.setState({ destinationInput: input });
+            this.getPlacesDebounced(input);
+          }}
           style={placeInputStyle}
           placeholder="Where to?"
         />
